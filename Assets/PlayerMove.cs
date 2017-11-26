ea4b7571613c4354918e5d6a629d8b5f252cf3fb
Eric Marty <contact@ericmarty.co>
2017-11-26 12:17:10 -0800

    Player movement with raycast

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,47 +6,94 @@ public class PlayerMove : MonoBehaviour {
 
 	public int playerSpeed = 10;
 	public bool facingRight = false;
-	public int playerJumpPower = 1250;
-	public float moveX;
+
+	[Header("Jump Mechanics")]
+	public int playerJumpPower = 100;
 	public bool isGrounded;
-	
+	public LayerMask groundLayer;
+	public float isGroundedBleed = 0;
+
+	float xMovement;
+
 	// Update is called once per frame
 	void Update () {
 		PlayerMovement();
 	}
 
+	bool IsGrounded()
+	{
+		Vector2 position = transform.position;
+		Vector2 direction = Vector2.down;
+		float distance = 1.0f;
+		float width = GetComponent<SpriteRenderer>().bounds.size.x;
+		float speedBasedBleed = 0.0f;
+		
+		// Allow ground bleed (larger jump hit width) if moving full speed
+		if (Mathf.Abs(xMovement) == 1)
+		{
+			 speedBasedBleed = isGroundedBleed;
+		}
+
+		Vector2 leftPosition = position - (new Vector2(width / 2 + speedBasedBleed, 0));
+		Vector2 rightPosition = position + (new Vector2(width / 2 + speedBasedBleed, 0));
+
+		// DEBUG
+		Debug.DrawRay(leftPosition, direction, Color.green, 5.0f);
+		Debug.DrawRay(rightPosition, direction, Color.green, 5.0f);
+		Debug.DrawRay(position, direction, Color.red, 5.0f);
+
+		RaycastHit2D centerHit = Physics2D.Raycast(position, direction, distance, groundLayer);
+		RaycastHit2D leftHit = Physics2D.Raycast(leftPosition, direction, distance, groundLayer);
+		RaycastHit2D rightHit = Physics2D.Raycast(rightPosition, direction, distance, groundLayer);
+
+		//Debug.Log(hit.distance);
+		//Debug.Log(hit.collider);
+
+		if (centerHit.collider != null || leftHit.collider != null || rightHit.collider != null)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
 	void PlayerMovement()
 	{
 		// Controls
-		moveX = Input.GetAxis("Horizontal");
-		if (Input.GetButtonDown("Jump") || Input.GetKeyDown("w"))
+		xMovement = Input.GetAxis("Horizontal");
+		bool jump = Input.GetButtonDown("Jump");
+
+		if (jump || Input.GetKeyDown("w"))
 		{
-			if (isGrounded)
-			{
-				Jump();
-			}
+			Jump();
 		}
 
 		// Animations
 
 		// Player direction
-		if (moveX < 0.0f && facingRight == false)
+		if (xMovement < 0.0f && facingRight == false)
 		{
 			FlipPlayer();
 		}
-		else if (moveX > 0.0f && facingRight == true)
+		else if (xMovement > 0.0f && facingRight == true)
 		{
 			FlipPlayer();
 		}
 
+	}
+
+	private void FixedUpdate()
+	{
 		// Physics
-		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMovement * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
 
 	void Jump()
 	{
-		GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
-		isGrounded = false;
+		if (IsGrounded())
+		{
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+		}
 	}
 
 	void FlipPlayer()
