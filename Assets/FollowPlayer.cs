3e1b8446dd40ad243dcb8658bef766a57b03f9e2
Eric Marty <contact@ericmarty.co>
2017-11-26 01:07:07 -0800

    Player movement rework

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public float xMin;
+	public float xMax;
+	public float yMin;
+	public float yMax;
+
+	GameObject player;
+
+	private void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	// Update is called once per frame
+	void Update () {
+		float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+		float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+		gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public int playerSpeed = 10;
+	public bool facingRight = false;
+	public int playerJumpPower = 1250;
+	public float moveX;
+	public bool isGrounded;
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMovement();
+	}
+
+	void PlayerMovement()
+	{
+		// Controls
+		moveX = Input.GetAxis("Horizontal");
+		if (Input.GetButtonDown("Jump") || Input.GetKeyDown("w"))
+		{
+			if (isGrounded)
+			{
+				Jump();
+			}
+		}
+
+		// Animations
+
+		// Player direction
+		if (moveX < 0.0f && facingRight == false)
+		{
+			FlipPlayer();
+		}
+		else if (moveX > 0.0f && facingRight == true)
+		{
+			FlipPlayer();
+		}
+
+		// Physics
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void Jump()
+	{
+		GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+		isGrounded = false;
+	}
+
+	void FlipPlayer()
+	{
+		facingRight = !facingRight;
+		Vector2 localScale = gameObject.transform.localScale;
+		localScale.x *= -1;
+		transform.localScale = localScale;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.name == "Tilemap_ground")
+		{
+			isGrounded = true;
+		}
+	}
+}
