581d9b4cf9dcb844c5539b0c347539d102885471
Eric Marty <contact@ericmarty.co>
2017-11-28 21:31:09 -0800

    Attach lead taker to player back

--- Assets/LeadTaker.cs
+++ Assets/LeadTaker.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class LeadTaker : MonoBehaviour {
 
 	public Vector3 offset;
-
+	
 	GameObject player;
+	PlayerMove playerHealth;
+	bool prevPlayerFacingRight;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealth = player.GetComponent<PlayerMove>();
+		prevPlayerFacingRight = playerHealth.facingRight;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		bool currFacingRight = playerHealth.facingRight;
+		if (prevPlayerFacingRight != currFacingRight)
+		{
+			FlipLeadTaker();
+		}
+		
+		// Attach lead taker to player's back
 		this.transform.position = player.transform.position + offset;
+
+		prevPlayerFacingRight = currFacingRight;
+	}
+
+	private void FlipLeadTaker()
+	{
+		offset = new Vector3(-offset.x, offset.y, offset.z);
 	}
 }
