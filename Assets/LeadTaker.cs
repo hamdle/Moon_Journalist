74a4e22252f6bee6c48803cf3e04f0d386c82c6e
Eric Marty <contact@ericmarty.co>
2017-11-28 21:12:37 -0800

    Setup for combat mechanic using pencil

---
+++ Assets/LeadTaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadTaker : MonoBehaviour {
+
+	public Vector3 offset;
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = player.transform.position + offset;
+	}
+}
