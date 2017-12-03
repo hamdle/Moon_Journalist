5d6d8c68f87beb494627a767090096bb4fe7c055
Eric Marty <contact@ericmarty.co>
2017-12-03 15:17:12 -0800

    Combat effects - flashing enemy when hit and microphone fade out

--- Assets/MicrophoneDeath.cs
+++ Assets/MicrophoneDeath.cs
@@ -4,24 +4,66 @@ using UnityEngine;
 
 public class MicrophoneDeath : MonoBehaviour {
 
-	float deathDelay = 1f;
+	float deathDelay = 0.5f;
+	SpriteRenderer sr;
+	Collider2D col;
+	Color currentColor;
+	bool fadeOut = false;
+	float timer = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+		sr = GetComponent<SpriteRenderer>();
+		col = GetComponent<Collider2D>();
+		currentColor = sr.color;
+		/*Debug.Log(Color.red);
+		Color color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
+		Debug.Log(color);
+		sr.color = color;
+		*/
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (fadeOut)
+		{
+			if (timer < deathDelay)
+			{
+				timer += Time.deltaTime;
+				currentColor.a = deathDelay - timer;
+				Debug.Log(currentColor.a);
+				sr.color = currentColor;
+			}
+		}
 	}
 
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
 		// todo setup fade to alpha based on deathDelay
-		//Color color = this.gameObject.GetComponent<Renderer>().material.color;
-
+		fadeOut = true;
+		col.enabled = false;
 		Invoke("KillSelf", deathDelay);
+	}
+
+	private void FadeOut()
+	{
+		//transform.position = startingPosition + offset;
+
+		/*
+		Debug.Log("Fade out...");
+		Color color = sr.color;
+		float waitTime = deathDelay * Time.deltaTime / 255;
+		Debug.Log(waitTime);
+
+		for (int x = 255; x > 0; x--)
+		{
+
+			color.a = x * waitTime;
+			Debug.Log(color);
+			sr.color = color;
+			yield return new WaitForSeconds(waitTime);
+		}
+		*/
 	}
 
 	private void KillSelf()
--- Assets/TakeDamage.cs
+++ Assets/TakeDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TakeDamage : MonoBehaviour {
 
 	public float killTimer;
+	[Range(0,1)] public float flashTimer;
 
 	// Use this for initialization
 	void Start () {
@@ -20,9 +21,26 @@ public class TakeDamage : MonoBehaviour {
 	{
 		if (collision.gameObject.CompareTag("Projectile"))
 		{
-			collision.collider.enabled = false;
+			// Disable all further collisions
+			//collision.collider.enabled = false;
+			// Flash sprite to indicate hit
+			StartCoroutine(Flash(this.gameObject, flashTimer));
+			// Destroy object after killTimer seconds
 			Invoke("Kill", killTimer);
 		}
+	}
+
+	private IEnumerator Flash(GameObject gameObject, float flashTimer)
+	{
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+		//for (int i = 0; i < 10; i++)
+		//{
+			spriteRenderer.enabled = false;
+			yield return new WaitForSeconds(flashTimer);
+			spriteRenderer.enabled = true;
+			yield return new WaitForSeconds(flashTimer);
+		//}
 	}
 
 	private void Kill()
