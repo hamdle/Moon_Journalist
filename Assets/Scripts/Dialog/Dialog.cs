36608f189dda0ec53e09c6b697498fc9c4f00e2b
Eric Marty <contact@ericmarty.co>
2017-12-03 21:08:38 -0800

    Start dialog buildout

---
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog {
+
+	public string name;
+	// The senetences we will load into the queue
+	[TextArea(3, 10)]
+	public string[] sentences;
+}
---
+++ Assets/Scripts/Dialog/StoryElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryElement : MonoBehaviour {
+
+	public Dialog dialog;
+
+	// A way to feed this to the dialog manager
+	public void TriggerDialog()
+	{
+		FindObjectOfType<DialogManager>().StartDialog(dialog);
+	}
+}
