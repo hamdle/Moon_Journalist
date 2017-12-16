10d097ed26f6d259fb5d7ce53371a1e05e791055
Eric Marty <contact@ericmarty.co>
2017-12-15 20:56:12 -0800

    Remove missile code to start from scratch

--- Assets/MissileCommand.cs
+++ Assets/MissileCommand.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class MissileCommand : MonoBehaviour {
 
-	public float launchDistance = 5.0f;
+	public float launchTriggerDistance = 5.0f;
 
-	[SerializeField]
-	float initialAngle;
+	[SerializeField] Vector2 fireForce;
 
 	bool inRange = false;
 	bool fire = false;
+	bool firing = false;
 	Transform other;
 	Rigidbody2D rb;
 
@@ -22,7 +22,7 @@ public class MissileCommand : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (inRange)
+		if (inRange && !firing)
 		{
 			CheckTargetDistance();
 		}
@@ -32,36 +32,10 @@ public class MissileCommand : MonoBehaviour {
 	{
 		if (fire)
 		{
-			var rigid = rb;
+			firing = true;
 
-			Vector3 p = other.position;
+			rb.AddForce(fireForce);
 
-			float gravity = Physics.gravity.magnitude;
-			// Selected angle in radians
-			float angle = initialAngle * Mathf.Deg2Rad;
-
-			// Positions of this object and the target on the same plane
-			Vector3 planarTarget = new Vector3(p.x, 0, p.z);
-			Vector3 planarPostion = new Vector3(transform.position.x, 0, transform.position.z);
-
-			// Planar distance between objects
-			float distance = Vector3.Distance(planarTarget, planarPostion);
-			// Distance along the y axis between objects
-			float yOffset = transform.position.y - p.y;
-
-			float initialVelocity = (1 / Mathf.Cos(angle)) * Mathf.Sqrt((0.5f * gravity * Mathf.Pow(distance, 2)) / (distance * Mathf.Tan(angle) + yOffset));
-
-			Vector3 velocity = new Vector3(0, initialVelocity * Mathf.Sin(angle), initialVelocity * Mathf.Cos(angle));
-
-			// Rotate our velocity to match the direction between the two objects
-			float angleBetweenObjects = Vector3.Angle(Vector3.forward, planarTarget - planarPostion);
-			Vector3 finalVelocity = Quaternion.AngleAxis(angleBetweenObjects, Vector3.up) * velocity;
-
-			// Fire!
-			rigid.velocity = finalVelocity;
-
-			// Alternative way:
-			// rigid.AddForce(finalVelocity * rigid.mass, ForceMode.Impulse);
 			fire = false;
 		}
 	}
@@ -81,7 +55,7 @@ public class MissileCommand : MonoBehaviour {
 		if (other)
 		{
 			float dist = Vector3.Distance(other.position, transform.position);
-			if (dist < launchDistance)
+			if (dist < launchTriggerDistance)
 			{
 				fire = true;
 				Debug.Log("HIT");
