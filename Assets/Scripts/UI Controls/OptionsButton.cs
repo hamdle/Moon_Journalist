eab1ade3bbc07c1ccc0f9982249916f57b7ad2a7
Eric Marty <contact@ericmarty.co>
2017-12-24 17:32:03 -0800

    Start screen menu system

---
+++ Assets/Scripts/UI Controls/OptionsButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsButton : MonoBehaviour {
+
+	public GameObject optionsMenu;
+
+	private bool showOptionsMenu;
+
+	// Use this for initialization
+	void Start () {
+		showOptionsMenu = false;
+		optionsMenu.SetActive(showOptionsMenu);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToggleOptionsMenu()
+	{
+		showOptionsMenu = !showOptionsMenu;
+		optionsMenu.SetActive(showOptionsMenu);
+	}
+}
