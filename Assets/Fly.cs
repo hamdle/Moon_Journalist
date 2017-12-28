4f729e6d6eee553e997c2cb5eb039dab901ebfc2
Eric Marty <contact@ericmarty.co>
2017-12-28 06:08:14 -0800

    Add flying script for rocket build

---
+++ Assets/Fly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour {
+
+	public float horSpeed;
+	public float vertSpeed;
+	public float amplitude;
+
+	public Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+		tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		tempPosition.x += horSpeed;
+		tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * vertSpeed) * amplitude;
+		transform.position = tempPosition;
+	}
+}
