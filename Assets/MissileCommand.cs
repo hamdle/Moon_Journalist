893329e129e4a0aefd4335488a5960d8372da063
Eric Marty <contact@ericmarty.co>
2017-12-31 15:11:38 -0800

    Program rocket states and add rotating platform

--- Assets/MissileCommand.cs
+++ Assets/MissileCommand.cs
@@ -17,27 +17,60 @@ public class MissileCommand : MonoBehaviour {
 	Transform other;
 	Rigidbody2D rb;
 
+	// Track stages of the rocket launch
+	bool fallingBackDown = false;
+
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody2D>();
 		other = GameObject.FindGameObjectWithTag("Player").transform;
-
 		fireAudioSource.clip = fireSound;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// STAGE 1: Handle when to fire
 		if (inRange && !firing)
 		{
+			// Only do the extra target processing
+			// if the rocket is visible to the player
 			CheckTargetDistance();
 		}
 
-		// todo
-		// If coming rocket lists towards layer
+		// STAGE 2: Firing up. Check for falling
+		if (firing && !fallingBackDown)
+		{
+			// Triggered once when starts falling
+			if (rb.velocity.y < 0)
+			{
+				fallingBackDown = true;
+				transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+			}
+		}
 
-		// First hit after coming down will cause the bomb to explose
+		// STAGE 3: Is falling
+		if (fallingBackDown)
+		{
+			// Move towards player while falling
+			float x = transform.position.x;
+			float px = other.transform.position.x;
+			// Start with a base of 1
+			float moveFactor = 1f;
+			int moveFactorStep = 4;
 
-		// Hit while going up will defuse the bomb
+			if (x > px)
+			{
+				// For every 5 x distance add 1 to the base moveFactor
+				moveFactor += Mathf.Abs(Mathf.Round((px - x) / moveFactorStep));
+				transform.position = new Vector3(transform.position.x - moveFactor * Time.deltaTime, transform.position.y, transform.position.z);
+			}
+			else
+			{
+				moveFactor += Mathf.Abs(Mathf.Round((x - px) / moveFactorStep));
+				transform.position = new Vector3(transform.position.x + moveFactor * Time.deltaTime, transform.position.y, transform.position.z);
+			}
+		}
+		
 	}
 
 	private void FixedUpdate()
@@ -48,6 +81,7 @@ public class MissileCommand : MonoBehaviour {
 
 			rb.AddRelativeForce(fireForce);
 
+			// The rocket has been fired
 			fire = false;
 		}
 	}
@@ -60,6 +94,11 @@ public class MissileCommand : MonoBehaviour {
 	private void OnBecameInvisible()
 	{
 		inRange = false;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		// Todo: explode
 	}
 
 	void CheckTargetDistance()
