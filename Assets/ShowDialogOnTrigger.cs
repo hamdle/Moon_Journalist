f6266000ee4d5da2f9915b779a8423355ec13d19
Eric Marty <contact@ericmarty.co>
2018-01-01 23:01:46 -0800

    Make dialog trigger box and prefab

---
+++ Assets/ShowDialogOnTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDialogOnTrigger : MonoBehaviour {
+
+	public GameObject dialogBox;
+	ShowHideDialog shDialog;
+	StoryElement dialog;
+	bool toggled = false;
+
+	// Use this for initialization
+	void Start () {
+		shDialog = GetComponent<ShowHideDialog>();
+		dialog = GetComponent<StoryElement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (!toggled)
+		{
+			if (collision.gameObject.tag.Equals("Player"))
+			{
+				dialog.TriggerDialog();
+				shDialog.ShowHideToggle(dialogBox);
+				toggled = true;
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		toggled = false;
+	}
+}
