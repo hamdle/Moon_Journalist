544aba4e43e4cdcdacfa4cdc5acd6c59713aa8da
Eric Marty <contact@ericmarty.co>
2018-01-09 20:01:43 -0800

    Create and hook up level select buttons

---
+++ Assets/LoadOnButtonPress.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnButtonPress : MonoBehaviour {
+
+	public int sceneIndex;
+
+	public void LoadScene()
+	{
+
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
